ff1590d389f36531ab9fc1f8819fb587923c332b
manojkumar <manoj.kumar@innoventes.co>
2020-10-31 12:54:44 +0530

    token fetch

--- Desktop/dorabhai-assesment/src/App.js
+++ Desktop/dorabhai-assesment/src/App.js
@@ -9,27 +9,35 @@ class App extends Component {
     }
   }
 
-clicked=()=>{
-  alert("hai")
-}
+  clicked = () => {
+    console.log(this.state.username + this.state.password);
+    fetch('http://65.0.14.105/API/admin/adminLogin.php', {
+      method: "post",
+      body: JSON.stringify(this.state)
+    }).then((response) => {
+      response.json().then((result) => {
+        console.log("result", result)
+      })
+    })
+  }
 
   render() {
     return (
 
-      
-        <div className="main">
-         
-          <div className="main-header">
-            <h1>Login Form</h1>
-            <hr />
-            <h3>Welcome to Login</h3>
-            <p><input type="text" placeholder="username" onChange={(event, newname) => { this.setState({ username: newname }) }} /></p>
-            <p><input type="password" placeholder="password" onChange={(event, newname) => { this.setState({ password: newname }) }} /></p>
-            <p><button onClick={this.clicked}>continue</button></p>
-          </div>
-         
+
+      <div className="main">
+
+        <div className="main-header">
+          <h1>Login Form</h1>
+          <hr />
+          <h3>Welcome to Login</h3>
+          <p><input type="text" placeholder="username" onChange={(e) => { this.setState({ username: e.target.value }) }} /></p>
+          <p><input type="password" placeholder="password" onChange={(e) => { this.setState({ password: e.target.value }) }} /></p>
+          <p><button onClick={this.clicked}>continue</button></p>
         </div>
-     
+
+      </div>
+
 
     )
   }
